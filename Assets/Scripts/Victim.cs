bad98310e1514d3eddf97c12fac2f0b56c7e168b
Maxime Richard <maxime.richard06@gmail.com>
2016-01-30 10:23:40 +0100

    Civil random movement

--- Assets/Scripts/Victim.cs
+++ Assets/Scripts/Victim.cs
@@ -1,17 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class Script : MonoBehaviour {
+public class Victim : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float objectSpeed;
+    private Rigidbody2D rb;
+    private float xActive, yActive;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        xActive = 1.0f;
+        yActive = -1.0f;
+        InvokeRepeating("SwitchDirections", 0.5f, 1.0f);
+    }
+    void SwitchDirections()
+    {
+        xActive = Random.Range(-1.0f, 1.0f);
+        yActive = Random.Range(-1.0f, 1.0f);
+    }
+
+    void FixedUpdate()
+    {
+
+        rb.velocity = new Vector3(xActive, yActive, 0.0f) * objectSpeed;
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, -3.5f, 3.5f),
+            Mathf.Clamp(rb.position.y, -3.0f, 4.0f),
+            0
+            );
+    }
 
     void MoveAround()
     {
